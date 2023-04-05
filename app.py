c59f76c1776b9956958be2468b678117edd15a0f
Bishal <bishalbera2@gmail.com>
2023-04-05 10:00:11 +0530

    app added

---
+++ app.py
@@ -0,0 +1,31 @@
+import requests
+import os
+from datetime import datetime
+
+
+user_api = os.environ['api_key']
+location = input("Enter the city name: ")
+
+complete_api_link = "https://api.openweathermap.org/data/2.5/weather?q="+location+"&appid="+user_api
+
+api_link = requests.get(complete_api_link)
+api_data = api_link.json()
+
+if api_data['cod'] == '404':
+    print ("Invalid City: {}, Please check your city name".format(location))
+else:
+    temp_city = ((api_data['main']['temp']) - 273.15)
+    weather_desc = api_data['weather'][0]['description']
+    hmdt = api_data['main']['humidity']
+    wind_speed = api_data['wind']['speed']
+    date_time = datetime.now().strftime("%d %b %Y | %I:%M:%S %p")
+
+    print ("------------------------------------------------------------")
+    print ("Weather Stats for - {}  ||  {}".format(location.upper(), date_time))
+    print ("------------------------------------------------------------")
+    
+    print ("Current temperature is: {:.2f} deg C".format(temp_city))
+    print ("Current weather desc  :",weather_desc)
+    print ("Current humidity      :",hmdt)
+    print ("Current wind speed    :",wind_speed,'kmph')
+
